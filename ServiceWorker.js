51a4c6ff88b79e345c9f8063ba7e063e49000cfc
Vidak Mijanovic <vidak.mijanovic@gmail.com>
2026-03-11 09:20:40 +0100

    Version 0.2.3.

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Smiling Goat Studios-Space Invaders-0.2.2";
+const cacheName = "Smiling Goat Studios-Space Invaders-0.2.3";
 const contentToCache = [
     "Build/SpaceInvaders_Web.loader.js",
     "Build/SpaceInvaders_Web.framework.js.unityweb",
